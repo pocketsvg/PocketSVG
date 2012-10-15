7e99471df7d4ddc55cca01de6b890bb56a5d2b76
Ariel <arielelkin@gmail.com>
2012-10-15 18:58:02 +0100

    renamed svg files and placed PocketSVG files in root of directory

--- PocketSVG/ViewController.m
+++ PocketSVG/ViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
         
     //1: Create a PocketSVG object from your SVG file:
-    PocketSVG *myVectorDrawing = [[PocketSVG alloc] initFromSVGFileNamed:@"BezierCurve1-iPad"];
+    PocketSVG *myVectorDrawing = [[PocketSVG alloc] initFromSVGFileNamed:@"BezierCurve3"];
     
     
     //2: Its bezier property is the corresponding UIBezierPath:
