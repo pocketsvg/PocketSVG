dcd809f75124ffa6519441afa733d04029a0655a
Ariel Elkin <ariel@arivibes.com>
2013-03-29 00:10:40 +0100

    Replaced CC license by MIT license

--- PocketSVG.h
+++ PocketSVG.h
@@ -1,11 +1,25 @@
 //
 //  PocketSVG.h
 //
-//  Based on SvgToBezier.h, created by Martin Haywood on 5/9/11.
-//  Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0) license 2011 Ponderwell.
+//  Copyright (c) 2013 Ponderwell, Ariel Elkin, and Contributors
 //
-//  Cleaned up by Bob Monaghan - Glue Tools LLC 6 November 2011
-//  Integrated into PocketSVG 10 August 2012
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 #if TARGET_OS_IPHONE
