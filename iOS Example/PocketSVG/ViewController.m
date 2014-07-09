4b00b5227d32a563f18e0a888b8b7458bb1cc58d
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2014-07-09 16:46:22 +0900

    Updated demo and allowed running on iphone

--- iOS Example/PocketSVG/ViewController.m
+++ iOS Example/PocketSVG/ViewController.m
@@ -33,31 +33,41 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor blueColor];
+    
     //1: Turn your SVG into a CGPath:
-    NSString *svg = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"BezierCurve1" ofType:@"svg"]
+    NSString *svg = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"BezierCurve1"
+                                                                                       ofType:@"svg"]
                                           usedEncoding:NULL
                                                  error:NULL];
-    NSArray *paths = PSVGPathsFromSVGString(svg);
     
-    //2: To display it on screen, you can create a CAShapeLayer
-    //and set myPath as its path property:
-    CAShapeLayer *myShapeLayer = [CAShapeLayer layer];
-    myShapeLayer.path = (__bridge CGPathRef)paths[0];
-    
-    
-    //3: Fiddle with it using CAShapeLayer's properties:
-    myShapeLayer.strokeColor = [[UIColor redColor] CGColor];
-    myShapeLayer.lineWidth = 1;
-    myShapeLayer.fillColor = [[UIColor greenColor] CGColor];
-    
-    
-    //4: Display it!
-    [self.view.layer addSublayer:myShapeLayer];
+    NSMapTable *attributes;
+    NSArray *paths = PSVGPathsFromSVGString(svg, &attributes);
+    NSLog(@"%@", PSVGFromPaths(paths, nil));
+
+    for(id path in paths) {
+        // 2: To display it on screen, you can create a CAShapeLayer:
+        CAShapeLayer *layer = [CAShapeLayer layer];
+        layer.path = (__bridge CGPathRef)path;
+        
+        // 3: Fiddle with it using CAShapeLayer's properties:
+        NSDictionary *attrs = [attributes objectForKey:path];
+        layer.strokeColor = (__bridge CGColorRef)attrs[@"stroke"]
+                          ?:[[UIColor redColor] CGColor];
+        layer.lineWidth   = [attrs[@"stroke-width"] floatValue];
+        layer.fillColor   = (__bridge CGColorRef)attrs[@"fill"]
+                          ?: [[UIColor blueColor] CGColor];
+        
+        //4: Display it!
+        [self.view.layer addSublayer:layer];
+    }
     
     
 }
 
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
+}
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
