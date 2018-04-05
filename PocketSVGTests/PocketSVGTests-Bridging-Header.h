3e6e308e7a4172346af39d4106d21ba33079d58f
Scott Talbot <stalbot@scentregroup.com>
2018-04-05 18:07:30 +1000

    Test handling of optional transformation arguments

---
+++ PocketSVGTests/PocketSVGTests-Bridging-Header.h
@@ -0,0 +1,14 @@
+/*
+ * This file is part of the PocketSVG package.
+ * Copyright (c) Ponderwell, Ariel Elkin, Fjölnir Ásgeirsson, and Contributors
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+#ifndef PocketSVGTests_Bridging_Header_h
+#define PocketSVGTests_Bridging_Header_h
+
+#import "SVGEngine.h"
+
+#endif /* PocketSVGTests_Bridging_Header_h */
