987cfd616d364759c87ed2b3b3a50307c0ac52c8
Kwab Fordjour <kwabford@gmail.com>
2018-11-06 00:38:34 -0500

    CHG: Embed Complex Iceland SVG example in a zoomable scroll view.

--- Demos/Demo-iOS/Demo-iOS/IcelandicViewController.swift
+++ Demos/Demo-iOS/Demo-iOS/IcelandicViewController.swift
@@ -10,26 +10,25 @@ import UIKit
 import PocketSVG
 
 class IcelandicViewController: UIViewController {
+    
+    let icelandicView = UIView()
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
         let svgURL = Bundle.main.url(forResource: "iceland", withExtension: "svg")!
         let paths = SVGBezierPath.pathsFromSVG(at: svgURL)
-
-        let icelandicView = UIView()
-        icelandicView.frame = view.frame
-        icelandicView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-
+        
         for (index, path) in paths.enumerated() {
-
+            
             // assign different colours to different shapes:
             let shapeLayer = CAShapeLayer()
             shapeLayer.fillColor = UIColor(hue: CGFloat(index)/CGFloat(paths.count), saturation: 1, brightness: 1, alpha: 1).cgColor
             shapeLayer.strokeColor = UIColor(white: 1-CGFloat(index)/CGFloat(paths.count), alpha: 1).cgColor
             shapeLayer.path = path.cgPath
             icelandicView.layer.addSublayer(shapeLayer)
-
+            
             // animate stroke width:
             let animation = CABasicAnimation(keyPath: "lineWidth")
             animation.toValue = 4
@@ -41,19 +40,26 @@ class IcelandicViewController: UIViewController {
             animation.isRemovedOnCompletion = false
             shapeLayer.add(animation, forKey: animation.keyPath)
         }
-
-        // apply transforms to layer to place it within screen bounds:
-        var transform = CATransform3DMakeScale(0.4, 0.4, 1.0)
-        transform = CATransform3DTranslate(transform, -100, 0, 0)
-
-        icelandicView.layer.transform = transform
-
-        view.addSubview(icelandicView)
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
+        
+        let r = SVGBoundingRectForPaths(paths)
+        icelandicView.frame = r
+        
+        let scrollview = UIScrollView(frame: view.bounds)
+        scrollview.maximumZoomScale = 5.0
+        scrollview.minimumZoomScale = 0.5
+        
+        scrollview.contentSize = r.size
+        
+        scrollview.delegate = self
+        
+        scrollview.addSubview(icelandicView)
+        
+        view.addSubview(scrollview)
     }
 }
 
+extension IcelandicViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return icelandicView
+    }
+}
