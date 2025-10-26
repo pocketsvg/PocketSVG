b1ee4566cf9162ba2525cedfb1fb12497775d8ff
Ariel Elkin <1756909+arielelkin@users.noreply.github.com>
2025-10-26 10:23:46 +0100

    Fix Package manifest warning for Swift 6

--- Package.swift
+++ Package.swift
@@ -2,38 +2,57 @@
 
 import PackageDescription
 
+let supportedPlatforms: [SupportedPlatform] = [
+    .macOS(.v10_10),
+    .iOS(.v9),
+    .tvOS(.v10),
+    .watchOS(.v3),
+    .visionOS(.v1),
+]
+
+let packageProducts: [Product] = [
+    .library(
+        name: "PocketSVG",
+        type: .dynamic,
+        targets: ["PocketSVG"]
+    ),
+]
+
+let packageTargets: [Target] = [
+    .target(
+        name: "PocketSVG",
+        dependencies: [],
+        path: "Sources",
+        resources: [
+            .process("Resources"),
+        ]
+    ),
+    .testTarget(
+        name: "PocketSVGTests",
+        dependencies: ["PocketSVG"],
+        path: "Tests",
+        resources: [
+            .process("Resources"),
+        ]
+    ),
+]
+
+#if swift(>=6.0)
 let package = Package(
     name: "PocketSVG",
-    platforms: [
-        .macOS(.v10_10),
-        .iOS(.v9),
-        .tvOS(.v10),
-        .watchOS(.v3),
-        .visionOS(.v1)
-    ],
-    products: [
-        .library(
-            name: "PocketSVG",
-            type: .dynamic,
-            targets: ["PocketSVG"])
-    ],
-    targets: [
-        .target(
-            name: "PocketSVG",
-            dependencies: [],
-            path: "Sources",
-            resources: [
-                .process("Resources"),
-            ]
-        ),
-        .testTarget(
-            name: "PocketSVGTests",
-            dependencies: ["PocketSVG"],
-            path: "Tests",
-            resources: [
-                .process("Resources"),
-            ]
-        ),
-    ],
+    platforms: supportedPlatforms,
+    products: packageProducts,
+    targets: packageTargets,
+    swiftLanguageModes: [.v5],
     cxxLanguageStandard: .cxx14
 )
+#else
+let package = Package(
+    name: "PocketSVG",
+    platforms: supportedPlatforms,
+    products: packageProducts,
+    targets: packageTargets,
+    swiftLanguageVersions: [.v5],
+    cxxLanguageStandard: .cxx14
+)
+#endif
