aa99d6151750b8d3f2426dcb5776103f4f918a98
Ariel Elkin <arielelkin@gmail.com>
2025-10-26 11:51:17 +0100

    fix build_xcframeworks.sh

--- ci/build_xcframework.sh
+++ ci/build_xcframework.sh
@@ -1,10 +1,3 @@
-#
-# This file is part of the PocketSVG package.
-#
-# Copyright (c) Ponderwell, Ariel Elkin, Fjölnir Ásgeirsson, and Contributors
-# For the full copyright and license information, please view the LICENSE
-# file that was distributed with this source code.
-#
 #!/usr/bin/env bash
 set -xeo pipefail
 
