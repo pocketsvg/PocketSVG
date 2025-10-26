908d7613f254376332cb906d6c0d4af2730e69bd
Ariel Elkin <arielelkin@gmail.com>
2025-10-26 11:37:36 +0100

    update package.swift

--- Package.swift
+++ Package.swift
@@ -2,57 +2,39 @@
 
 import PackageDescription
 
-let supportedPlatforms: [SupportedPlatform] = [
-    .macOS(.v10_13),
-    .iOS(.v12),
-    .tvOS(.v12),
-    .watchOS(.v4),
-    .visionOS(.v1),
-]
-
-let packageProducts: [Product] = [
-    .library(
-        name: "PocketSVG",
-        type: .static,
-        targets: ["PocketSVG"]
-    ),
-]
-
-let packageTargets: [Target] = [
-    .target(
-        name: "PocketSVG",
-        dependencies: [],
-        path: "Sources",
-        resources: [
-            .process("Resources"),
-        ]
-    ),
-    .testTarget(
-        name: "PocketSVGTests",
-        dependencies: ["PocketSVG"],
-        path: "Tests",
-        resources: [
-            .process("Resources"),
-        ]
-    ),
-]
-
-#if swift(>=6.0)
 let package = Package(
     name: "PocketSVG",
-    platforms: supportedPlatforms,
-    products: packageProducts,
-    targets: packageTargets,
-    swiftLanguageModes: [.v5],
-    cxxLanguageStandard: .cxx14
-)
-#else
-let package = Package(
-    name: "PocketSVG",
-    platforms: supportedPlatforms,
-    products: packageProducts,
-    targets: packageTargets,
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v12),
+        .tvOS(.v12),
+        .watchOS(.v4),
+        .visionOS(.v1),
+    ],
+    products: [
+        .library(
+            name: "PocketSVG",
+            targets: ["PocketSVG"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "PocketSVG",
+            dependencies: [],
+            path: "Sources",
+            resources: [
+                .process("Resources"),
+            ]
+        ),
+        .testTarget(
+            name: "PocketSVGTests",
+            dependencies: ["PocketSVG"],
+            path: "Tests",
+            resources: [
+                .process("Resources"),
+            ]
+        ),
+    ],
     swiftLanguageVersions: [.v5],
     cxxLanguageStandard: .cxx14
 )
-#endif
